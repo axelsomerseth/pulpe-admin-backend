b811599da1ffaf7a8a2682dcd676d50b1641b246
Axel Somerseth Córdova López <axelsomerseth@gmail.com>
2022-10-20 23:45:05 -0600

    added views

--- source/app.ts
+++ source/app.ts
@@ -1,16 +1,16 @@
-// import path from "path";
+import path from "path";
 import express, { Express } from "express";
 import cors from "cors";
 import logger from "morgan";
 import compression from "compression";
 import helmet from "helmet";
-// import { engine } from "express-handlebars";
+import { engine } from "express-handlebars";
 
 // import routes
 // import indexRouter from "./routes/index";
 // import categoriesRouter from "./routes/categories";
 // import productsRouter from "./routes/products";
-// import viewsRouter from "./routes/views";
+import viewsRouter from "./routes/views";
 
 const app: Express = express();
 
@@ -32,14 +32,14 @@ app.use(helmet()); // 3rd party plugin: secure your Express apps by setting vari
 // app.use("/categories", categoriesRouter);
 
 // static files
-// app.use("/static", express.static(path.join(__dirname, "public")));
+app.use("/static", express.static(path.join(__dirname, "public")));
 
 // view/template engine
-// app.engine("handlebars", engine());
-// app.set("view engine", "handlebars");
-// app.set("views", path.join(__dirname, "views"));
-// app.enable("view cache");
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "views"));
+app.enable("view cache");
 
-// app.use("/views", viewsRouter);
+app.use("/views", viewsRouter);
 
 export { app };
