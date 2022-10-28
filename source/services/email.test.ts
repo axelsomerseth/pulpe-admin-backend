327d3145102a7c046f32cea47c5443eaf2af0358
Axel Somerseth Córdova López <axelsomerseth@gmail.com>
2022-10-28 15:50:48 -0600

    test email sender happy path

---
+++ source/services/email.test.ts
@@ -0,0 +1,22 @@
+import { EmailSenderSMTP } from "./email";
+
+describe("email service", () => {
+  // happy path
+  it("should send emails", () => {
+    // arrange
+    const emailOptions = {
+      from: "Test Username <test@email.test>",
+      to: "axelsomerseth@gmail.com",
+      subject: "Test email",
+      textBody: "This is a test",
+    };
+
+    // act
+    const emailSender = new EmailSenderSMTP();
+    const outcome = emailSender.send(emailOptions);
+
+    // assert
+    expect(emailSender).toBeDefined();
+    expect(outcome).toBeDefined();
+  });
+});
