c687edb43c6a0172a38019f64d891e9f1fdba5c0
Axel Somerseth Córdova López <axelsomerseth@gmail.com>
2022-10-31 00:22:55 -0600

    removing test-email route

--- source/routes/index.ts
+++ source/routes/index.ts
@@ -1,10 +1,8 @@
 import express, { Router } from "express";
 import { indexHandler } from "../handlers/index";
-import { emailHandler } from "../handlers/email";
 
 const router: Router = express.Router();
 
 router.get("/", indexHandler);
-router.get("/test-email", emailHandler);
 
 export default router;
