ba914792c07f78acbad200c8c9ec3d3605766bf5
Axel Somerseth Cordova <axelsomerseth@gmail.com>
2022-11-15 22:43:10 -0600

    joi schemas updated

--- source/schemas/category.ts
+++ source/schemas/category.ts
@@ -2,12 +2,12 @@ import Joi from "joi";
 
 const categorySchema = {
   create: Joi.object({
-    name: Joi.string().max(60).required(),
-    description: Joi.string().max(255),
+    name: Joi.string().max(150).required(),
+    description: Joi.string(),
   }),
   update: Joi.object({
-    name: Joi.string().max(60).required(),
-    description: Joi.string().max(255),
+    name: Joi.string().max(150).required(),
+    description: Joi.string(),
   }),
 };
 
--- source/schemas/product.ts
+++ source/schemas/product.ts
@@ -2,15 +2,15 @@ import Joi from "joi";
 
 const productSchema = {
   create: Joi.object({
-    name: Joi.string().max(100).required(),
-    description: Joi.string().max(255).required(),
+    name: Joi.string().max(150).required(),
+    description: Joi.string().required(),
     categoryId: Joi.number().required(),
     price: Joi.number().required(),
     stock: Joi.number().required(),
   }),
   update: Joi.object({
-    name: Joi.string().max(100).required(),
-    description: Joi.string().max(255).required(),
+    name: Joi.string().max(150).required(),
+    description: Joi.string().required(),
     categoryId: Joi.number().required(),
     price: Joi.number().required(),
     stock: Joi.number().required(),
