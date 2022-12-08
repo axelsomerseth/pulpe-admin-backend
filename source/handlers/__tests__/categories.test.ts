eca96a01d581d918e4c0e3a54089712e4f719996
Axel Somerseth Cordova <axelsomerseth@gmail.com>
2022-12-08 17:44:36 -0600

    created category handler test

---
+++ source/handlers/__tests__/categories.test.ts
@@ -0,0 +1,60 @@
+import { Request, Response, NextFunction } from "express";
+import { Category } from "../../db/entities/categories";
+import {
+  listCategories,
+  readCategory,
+  createCategory,
+  updateCategory,
+  deleteCategory,
+} from "../categories";
+
+jest.mock("typeorm");
+
+describe("categories request handler", () => {
+  it("should list categories", async () => {
+    // arrange
+    const page = 1;
+    const size = 1;
+    const req = {
+      query: {
+        page,
+        size,
+      },
+    } as unknown as Request;
+    const res = { send: jest.fn(), status: jest.fn() } as unknown as Response;
+    const next = jest.fn() as NextFunction;
+
+    // act
+    await listCategories(req, res, next);
+
+    // assert
+    expect(res.send).toHaveBeenCalledTimes(1);
+  });
+
+  it.todo("should import multiple categories");
+
+  it("should create one category", async () => {
+    // arrange
+    const req = {
+      body: {
+        name: "Mock Name",
+        description: "Mock Description",
+      },
+    } as unknown as Request;
+    const res = { status: jest.fn(), send: jest.fn() } as unknown as Response;
+    const next = jest.fn() as NextFunction;
+
+    // act
+    await createCategory(req, res, next);
+
+    // assert
+    expect(res.status).toHaveBeenCalledTimes(1);
+    expect(res.send).toHaveBeenCalledTimes(1);
+  });
+
+  it.todo("should read one category");
+
+  it.todo("should update one category");
+
+  it.todo("should delete one category");
+});
